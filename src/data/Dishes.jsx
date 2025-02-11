ad3d48530899dd52b6716ebd9314db09acb0552b
Omzo85 <112520648+Omzo85@users.noreply.github.com>
2025-02-11 16:38:28 +0100

    11/02 apre

--- src/data/Dishes.jsx
+++ src/data/Dishes.jsx
@@ -2,14 +2,54 @@
 
 
 export const dishes = [
+  {
+    id: 'pastels',
+    name: 'Pastels au thon',
+    description: 'Beignets farcis au thon',
+    longDescription: 'Délicieux beignets croustillants farcis au thon frais, aux herbes et épices. Une entrée traditionnelle sénégalaise très appréciée.',
+    ingredients: 'Pâte à beignet, thon, oignons, persil, épices',
+    price: 6.90,
+    image: 'pastel_poisson.jpg',
+    category: 'entrees'
+  },
+  {
+    id: 'pastelsViande',
+    name: 'Pastels au Boeuf',
+    description: 'Beignets farcis au Boeuf',
+    longDescription: 'Délicieux beignets croustillants farcis au Boeuf, aux herbes et épices. Une entrée traditionnelle sénégalaise très appréciée.',
+    ingredients: 'Pâte à beignet, thon, oignons, persil, épices',
+    price: 6.90,
+    image: 'Pastels.jpeg',
+    category: 'entrees'
+  },
+  {
+    id: 'accras',
+    name: 'Accras de poisson',
+    description: 'Beignets de poisson épicés',
+    longDescription: 'Beignets croustillants à base de poisson blanc émietté et d\'épices. Une spécialité qui vous transportera directement en Afrique de l\'Ouest.',
+    ingredients: 'Poisson blanc, farine, oignons, piments, épices',
+    price: 7.50,
+    image: 'assets/img/accra.jpeg',
+    category: 'entrees'
+  },
+  {
+    id: 'salade-yassa',
+    name: 'Salade Yassa',
+    description: 'Salade d\'oignons confits au citron',
+    longDescription: 'Une salade rafraîchissante d\'oignons confits marinés au citron et aux épices. Un classique de la cuisine sénégalaise.',
+    ingredients: 'Oignons, citron, moutarde, huile d\'olive, épices',
+    price: 5.90,
+    image: 'assets/img/salade-yassa.jpeg',
+    category: 'entrees'
+  },
     {
-      id: 'thiep-poulet',
-      name: 'Thiepp au poulet',
-      description: 'Riz rouge au poulet rôti',
+      id: 'thiep-poisson',
+      name: 'Thiepp au poisson',
+      description: 'Riz rouge au poisson',
       longDescription: 'Un plat traditionnel sénégalais composé de riz cuit dans une sauce tomate riche, accompagné de poulet rôti mariné aux épices africaines.',
       ingredients: 'Riz, poulet, tomates, oignons, ail, poivrons, épices traditionnelles',
       price: 15.90,
-      image: '/yassa.jpeg',
+      image: '/thiepp.jpeg',
       category: 'plats',
      
       
@@ -27,17 +67,27 @@ export const dishes = [
     },
 
     {
-      id: 'yassa',
-      name: 'Yassa poulet',
+      id: 'thiere',
+      name: 'Couscous sénégalais',
       description: 'Poulet mariné au citron et aux oignons',
       longDescription: 'Une spécialité sénégalaise à base de poulet mariné dans une sauce aux oignons caramélisés et au citron, accompagnée de riz blanc.',
       ingredients: 'Poulet, oignons, citron, moutarde, ail, épices',
       price: 14.90,
-      image: '/yassa.jpeg',
+      image: '/thiéré.jpeg',
       category: 'plats'
       
     },
-
+    {
+      id: 'maffé',
+      name: 'Maffé au Boeuf',
+      description: 'Poulet mariné au citron et aux oignons',
+      longDescription: 'Voici l’histoire d’une recette : Le mafé ou maffé est une sauce typique africaine faite à base de pâte d’arachide plus communément appelée beurre de cacahuète. C’est un plat originaire du Mali : les premiers consommateurs sont les peuples mandingue et bambara. ',
+      ingredients: 'Poulet, oignons, citron, moutarde, ail, épices',
+      price: 14.90,
+      image: '/maffe.jpg',
+      category: 'plats'
+      
+    },
     
 
     // Ajoutez les autres plats ici...
