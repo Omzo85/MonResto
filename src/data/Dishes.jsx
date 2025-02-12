5e0f36058f8bf26c71f888c92015fefed99ae0fb
Omzo85 <112520648+Omzo85@users.noreply.github.com>
2025-02-12 11:31:44 +0100

    yes

--- src/data/Dishes.jsx
+++ src/data/Dishes.jsx
@@ -29,27 +29,18 @@ export const dishes = [
     longDescription: 'Beignets croustillants à base de poisson blanc émietté et d\'épices. Une spécialité qui vous transportera directement en Afrique de l\'Ouest.',
     ingredients: 'Poisson blanc, farine, oignons, piments, épices',
     price: 7.50,
-    image: 'assets/img/accra.jpeg',
+    image: 'accra.jpg',
     category: 'entrees'
   },
-  {
-    id: 'salade-yassa',
-    name: 'Salade Yassa',
-    description: 'Salade d\'oignons confits au citron',
-    longDescription: 'Une salade rafraîchissante d\'oignons confits marinés au citron et aux épices. Un classique de la cuisine sénégalaise.',
-    ingredients: 'Oignons, citron, moutarde, huile d\'olive, épices',
-    price: 5.90,
-    image: 'assets/img/salade-yassa.jpeg',
-    category: 'entrees'
-  },
+ 
     {
       id: 'thiep-poisson',
       name: 'Thiepp au poisson',
-      description: 'Riz rouge au poisson',
+      description: 'Riz rouge au poisson avec légumes du jour',
       longDescription: 'Un plat traditionnel sénégalais composé de riz cuit dans une sauce tomate riche, accompagné de poulet rôti mariné aux épices africaines.',
       ingredients: 'Riz, poulet, tomates, oignons, ail, poivrons, épices traditionnelles',
       price: 15.90,
-      image: '/thiepp.jpeg',
+      image: 'tpoisson.jpg',
       category: 'plats',
      
       
@@ -67,13 +58,13 @@ export const dishes = [
     },
 
     {
-      id: 'thiere',
-      name: 'Couscous sénégalais',
-      description: 'Poulet mariné au citron et aux oignons',
+      id: 'dibi',
+      name: 'Dibiterie',
+      description: 'Agneau braisé avec de attiéké et salades',
       longDescription: 'Une spécialité sénégalaise à base de poulet mariné dans une sauce aux oignons caramélisés et au citron, accompagnée de riz blanc.',
       ingredients: 'Poulet, oignons, citron, moutarde, ail, épices',
       price: 14.90,
-      image: '/thiéré.jpeg',
+      image: 'dibi.jpeg',
       category: 'plats'
       
     },
@@ -88,7 +79,28 @@ export const dishes = [
       category: 'plats'
       
     },
-    
+    {
+      id: 'tiacry',
+      name: 'Tiacry',
+      description: ' Couscous de mil, de babeurre ou de yaourt ou encore de lait caillé, de sucre vanillé, de beurre et de sucre raffiné1,2',
+      longDescription: 'Le dèguè ou thiakry est un dessert rafraîchissant d’origine ouest-africaine. Ce dessert est à base de couscous de mil et de yaourt (ou parfois de lait caillé).',
+      ingredients: 'Poulet, oignons, citron, moutarde, ail, épices',
+      price: 5.90,
+      image: 'tiacry.jpg',
+      category: 'desserts'
+      
+    },
+    {
+      id: 'ngalakh',
+      name: 'Ngalakh',
+      description: ' Couscous de mil, de babeurre ou de yaourt ou encore de lait caillé, de sucre vanillé, de beurre et de sucre raffiné1,2',
+      longDescription: 'Le dèguè ou thiakry est un dessert rafraîchissant d’origine ouest-africaine. Ce dessert est à base de couscous de mil et de yaourt (ou parfois de lait caillé).',
+      ingredients: 'Poulet, oignons, citron, moutarde, ail, épices',
+      price: 5.90,
+      image: 'ngalakh.jpg',
+      category: 'desserts'
+      
+    },
 
     // Ajoutez les autres plats ici...
   ];
